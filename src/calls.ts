98bf86a861dec7e99550c5727f3034ece052a0e7
ilya <ikazovsky@liveintent.com>
2023-07-18 19:05:06 +0200

    Only url in the error message

--- src/calls.ts
+++ src/calls.ts
@@ -2,9 +2,8 @@ import { CallHandler, isFunction } from 'live-connect-common'
 
 export class DefaultCallHandler implements CallHandler {
   ajaxGet(url: string, responseHandler: (responseText: string, response: object) => void, fallback?: (error: unknown) => void, timeout = 1000): void {
-    function errorCallback(name: string, message: string, error: unknown, request: XMLHttpRequest | XDomainRequest) {
-      console.error('Error while executing ajax call', message, error, request)
-      if (isFunction(fallback)) fallback(error)
+    function errorCallback(message: string) {
+      if (isFunction(fallback)) fallback(new Error(message))
     }
 
     function xhrCall(): XMLHttpRequest {
@@ -15,8 +14,7 @@ export class DefaultCallHandler implements CallHandler {
           if ((status >= 200 && status < 300) || status === 304) {
             responseHandler(xhr.responseText, xhr)
           } else {
-            const error = new Error(`Incorrect status received : ${status}`)
-            errorCallback('XHRError', `Error during XHR call: ${status}, url: ${url}`, error, xhr)
+            errorCallback(`Error during XHR call: ${status}, url: ${url}`)
           }
         }
       }
@@ -27,26 +25,20 @@ export class DefaultCallHandler implements CallHandler {
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       const xdr = new window.XDomainRequest!()
       xdr.onprogress = () => undefined
-      xdr.onerror = () => {
-        const error = new Error(`XDR Error received: ${xdr.responseText}`)
-        errorCallback('XDRError', `Error during XDR call: ${xdr.responseText}, url: ${url}`, error, xdr)
-      }
+      xdr.onerror = () => errorCallback(`Error during XDR call: ${xdr.responseText}, url: ${url}`)
       xdr.onload = () => responseHandler(xdr.responseText, xdr)
       return xdr
     }
 
     try {
       const request = (window && window.XDomainRequest) ? xdrCall() : xhrCall()
-      request.ontimeout = () => {
-        const error = new Error(`Timeout after ${timeout}, url : ${url}`)
-        errorCallback('AjaxTimeout', `Timeout after ${timeout}`, error, request)
-      }
+      request.ontimeout = () => errorCallback(`Timeout after ${timeout}, url : ${url}`)
       request.open('GET', url, true)
       request.timeout = timeout
       request.withCredentials = true
       request.send()
     } catch (error) {
-      errorCallback('AjaxCompositionError', `Error while constructing ajax request, ${error}`, error, undefined)
+      errorCallback(`Error while constructing ajax request, ${error}, url: ${url}`)
     }
   }
 
